a4fd616313af2eb907c2ed81411888505276b0ca
Modesty Ophelia Aguilar <ophelia.m.aguilar@gmail.com>
2022-10-28 16:10:30 +0200

    Initial Commit

---
+++ Foodtrucks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Foodtrucks
+//
+//  Created by Ophelia Aguilar on 2022-10-28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Foodtrucks/FoodtrucksApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodtrucksApp.swift
+//  Foodtrucks
+//
+//  Created by Ophelia Aguilar on 2022-10-28.
+//
+
+import SwiftUI
+
+@main
+struct FoodtrucksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
